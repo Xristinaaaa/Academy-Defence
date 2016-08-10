29eb054b48360014614ba49c0280325f08526259
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2016-08-10 20:35:06 +0300

    fix bullets

--- Scripts/Tower.js
+++ Scripts/Tower.js
@@ -15,7 +15,7 @@ function Tower(setType,setPos,setAttackSpeed){
          console.log(this.Position);
          if (distance2D(this.Position,new Vector2(bugs[i].X,bugs[i].Y)) < this.Range)
          {
-           bullets.push(new Bullet(bugs[i],this.Position,3,10,0));
+           bullets.push(new Bullet(bugs[i], new Vector2(this.Position.X, this.Position.Y),2,10,0));
            break;
          }
        }
@@ -38,9 +38,14 @@ function Bullet(bug,setPos,setSpeed,setDmg,setType){
   this.TraceBug = function(){
     //Todo: When bug dies and becomes undefined , remove this bullet//
      if (this.Clock % this.Speed === 0 || this.Clock % this.Speed === -0){
+      var prevPosition = new Vector2(this.Position.X, this.Position.Y);
       var moveVector = getDirection(this.Position,new Vector2(this.TrackedBug.X,this.TrackedBug.Y));
       this.Position.X += moveVector.X * ( (Resolution.X*blockSize) / this.Speed );
       this.Position.Y += moveVector.Y * ( (Resolution.X*blockSize) / this.Speed );
+      if ( distance2D(prevPosition, new Vector2(this.TrackedBug.X,this.TrackedBug.Y)) < 
+            distance2D(this.Position, new Vector2(this.TrackedBug.X,this.TrackedBug.Y))) {
+              bullets.splice(bullets.indexOf(this), 1);
+      }
      }
      this.Clock += 1;
   }
