510dffe786eade2fc26267a8a6af36ffeb1e0a3c
tebeshira <teodor_d@abv.bg>
2016-08-10 15:51:07 +0300

    adding new map level as a comment in map.js

--- Scripts/Map.js
+++ Scripts/Map.js
@@ -43,4 +43,37 @@ function setRoadForLevel() {
     grida[7][6].Type = 1; grida[7][6].Direction = new Vector2(0, 1);
     grida[8][6].Type = 1; grida[8][6].Direction = new Vector2(1, 0);
     grida[8][7].Type = 1; grida[8][7].Direction = new Vector2(1, 0);
-}
+} 
+
+/*  Setting the path - level 2
+
+    grida[8][0].Type = 1; grida[8][0].Direction = new Vector2(1, 0);
+    grida[8][1].Type = 1; grida[8][1].Direction = new Vector2(1, 0);
+    grida[8][2].Type = 1; grida[8][2].Direction = new Vector2(1, 0);
+    grida[8][3].Type = 1; grida[8][3].Direction = new Vector2(1, 0);
+    grida[8][4].Type = 1; grida[8][4].Direction = new Vector2(1, 0);
+    grida[8][5].Type = 1; grida[8][5].Direction = new Vector2(1, 0);
+    grida[8][6].Type = 1; grida[8][6].Direction = new Vector2(1, 0);
+    grida[8][7].Type = 1; grida[8][7].Direction = new Vector2(1, 0);
+    grida[8][8].Type = 1; grida[8][8].Direction = new Vector2(0, -1);
+    grida[7][8].Type = 1; grida[7][8].Direction = new Vector2(0, -1);
+    grida[6][8].Type = 1; grida[6][8].Direction = new Vector2(0, -1);
+    grida[5][8].Type = 1; grida[5][8].Direction = new Vector2(-1, 0);
+    grida[5][7].Type = 1; grida[5][7].Direction = new Vector2(-1, 0);
+    grida[5][6].Type = 1; grida[5][6].Direction = new Vector2(-1, 0);
+    grida[5][5].Type = 1; grida[5][5].Direction = new Vector2(-1, 0);
+    grida[5][4].Type = 1; grida[5][4].Direction = new Vector2(-1, 0);
+    grida[5][3].Type = 1; grida[5][3].Direction = new Vector2(-1, 0);
+    grida[5][2].Type = 1; grida[5][2].Direction = new Vector2(-1, 0);
+    grida[5][1].Type = 1; grida[5][1].Direction = new Vector2(0, -1);
+    grida[4][1].Type = 1; grida[4][1].Direction = new Vector2(0, -1);
+    grida[3][1].Type = 1; grida[3][1].Direction = new Vector2(0, -1);
+    grida[2][1].Type = 1; grida[2][1].Direction = new Vector2(0, -1);
+    grida[1][1].Type = 1; grida[1][1].Direction = new Vector2(1, 0);
+    grida[1][2].Type = 1; grida[1][2].Direction = new Vector2(1, 0);
+    grida[1][3].Type = 1; grida[1][3].Direction = new Vector2(1, 0);
+    grida[1][4].Type = 1; grida[1][4].Direction = new Vector2(1, 0);
+    grida[1][5].Type = 1; grida[1][5].Direction = new Vector2(1, 0);
+    grida[1][6].Type = 1; grida[1][6].Direction = new Vector2(1, 0);
+    grida[1][7].Type = 1; grida[1][7].Direction = new Vector2(1, 0);
+} */
